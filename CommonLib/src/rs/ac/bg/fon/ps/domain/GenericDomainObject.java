baeff487c1e59a2f65b115e0be065898c8a600a8
JankovicN <nj20180199@student.fon.bg.ac.rs>
2022-03-20 15:09:04 +0100

    Creating interface GenericDomainObject and its methods

---
+++ CommonLib/src/rs/ac/bg/fon/ps/domain/GenericDomainObject.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package rs.ac.bg.fon.ps.domain;
+
+import java.io.Serializable;
+import java.util.List;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author nikol
+ */
+public interface GenericDomainObject extends Serializable{
+    
+    String getTableName();// returns String representing the name of the table
+    String getColumnNamesForInsert();// returns String representing column names that are used for INSERT query
+    String getColumnNamesForInsertWithAlias();// returns String representing column names with alias that are used for INSERT query
+    String getDeleteCondition();// returns String representing the condition for DELETE query
+    String getUpdateCondition();// returns String representing the condition for UPDATE query
+    String getUpdateValues(GenericDomainObject gdo);// returns String representing values that are changing in UPDATE query
+    String getInsertValues();// returns String representing values that are inserted in table, in INSERT queey
+    String getAlias();//returns alias for SQL query
+    String getSecondAlias();
+    String getValuesForDoubleJoin();// returns String representing values when joining three tables
+    String getForeignKey();//returns foreign key as String
+    String getSecondForeignKey();//returns second foreign key as String, if the table has two foreign keys
+    String getPrimaryKeyColumnName();//returns the name of the column of primary key
+    String getSecondPrimarykeyColumnName();//returns the name of the column of second primary key, if the table has two primary keys
+    List<GenericDomainObject> readResultSet(ResultSet rs) throws Exception; // reads ResultSet and returns list of objects
+    
+}
