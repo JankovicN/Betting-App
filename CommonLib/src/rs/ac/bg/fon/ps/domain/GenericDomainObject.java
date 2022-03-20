386c3ccf9c8e1cc7de009c2aa3c96bc887b4cc19
JankovicN <nj20180199@student.fon.bg.ac.rs>
2022-03-20 15:18:25 +0100

    Update GenericDomainObject.java

--- CommonLib/src/rs/ac/bg/fon/ps/domain/GenericDomainObject.java
+++ CommonLib/src/rs/ac/bg/fon/ps/domain/GenericDomainObject.java
@@ -14,20 +14,20 @@ import java.sql.ResultSet;
  */
 public interface GenericDomainObject extends Serializable{
     
-    String getTableName();// returns String representing the name of the table
-    String getColumnNamesForInsert();// returns String representing column names that are used for INSERT query
-    String getColumnNamesForInsertWithAlias();// returns String representing column names with alias that are used for INSERT query
-    String getDeleteCondition();// returns String representing the condition for DELETE query
-    String getUpdateCondition();// returns String representing the condition for UPDATE query
-    String getUpdateValues(GenericDomainObject gdo);// returns String representing values that are changing in UPDATE query
-    String getInsertValues();// returns String representing values that are inserted in table, in INSERT queey
-    String getAlias();//returns alias for SQL query
+    String getTableName(); // returns String representing the name of the table
+    String getColumnNamesForInsert(); // returns String representing column names that are used for INSERT query
+    String getColumnNamesForInsertWithAlias(); // returns String representing column names with alias that are used for INSERT query
+    String getDeleteCondition(); // returns String representing the condition for DELETE query
+    String getUpdateCondition(); // returns String representing the condition for UPDATE query
+    String getUpdateValues(GenericDomainObject gdo); // returns String representing values that are changing in UPDATE query
+    String getInsertValues(); // returns String representing values that are inserted in table, in INSERT queey
+    String getAlias(); //returns alias for SQL query
     String getSecondAlias();
-    String getValuesForDoubleJoin();// returns String representing values when joining three tables
-    String getForeignKey();//returns foreign key as String
-    String getSecondForeignKey();//returns second foreign key as String, if the table has two foreign keys
-    String getPrimaryKeyColumnName();//returns the name of the column of primary key
-    String getSecondPrimarykeyColumnName();//returns the name of the column of second primary key, if the table has two primary keys
+    String getValuesForDoubleJoin(); // returns String representing values when joining three tables
+    String getForeignKey(); // returns foreign key as String
+    String getSecondForeignKey(); // returns second foreign key as String, if the table has two foreign keys
+    String getPrimaryKeyColumnName(); // returns the name of the column of primary key
+    String getSecondPrimarykeyColumnName(); // returns the name of the column of second primary key, if the table has two primary keys
     List<GenericDomainObject> readResultSet(ResultSet rs) throws Exception; // reads ResultSet and returns list of objects
     
 }
